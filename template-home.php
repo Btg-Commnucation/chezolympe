def3b0636690a564176bcfa0aebf5e1807cf5b45
Tuvedunyel <mightygodx@gmail.com>
2022-12-15 16:39:43 +0100

    home init

---
+++ template-home.php
@@ -0,0 +1,15 @@
+<?php
+/*
+* Template Name: Home
+*/
+
+get_header(); ?>
+
+<main id="home">
+    <section class="hero-banner">
+        <div class="leaf-top"></div>
+        <h1>Le blog d'Olympe</h1>
+    </section>
+</main>
+
+<?php get_footer(); ?>
