93ba96fb3c4222d2503c2c56bd54ac73daca7707
Tuvedunyel <mightygodx@gmail.com>
2022-12-15 14:21:33 +0100

    single resposive

---
+++ parts/bottom.php
@@ -0,0 +1,79 @@
+<section class="instagram">
+    <div class="title-container">
+        <h2>Nous suivre sur instagram</h2>
+        <p>Suis-nous sur Instagram et retrouve tous nos conseils</p>
+    </div>
+    <ul class="insta-image">
+        <li>
+            <a href="https://www.instagram.com/chez.olympe/" target="_blank" title="Voir le post instagram" rel="noreferrer noopener">
+                <span class="screen-reader-text">
+                    Description instagram
+                </span>
+                <img src="<?= get_template_directory_uri(); ?>/img/insta-un.jpg" alt="Photo d'une femme se cachant les seins">
+            </a>
+        </li>
+        <li>
+            <a href="https://www.instagram.com/chez.olympe/" target="_blank" title="Voir le post instagram" rel="noreferrer noopener">
+                <span class="screen-reader-text">
+                    Description instagram
+                </span>
+                <img src="<?= get_template_directory_uri(); ?>/img/insta-trois.jpg" alt="Deux femmes s'embrassant en chemise">
+            </a>
+        </li>
+        <li>
+            <a href="https://www.instagram.com/chez.olympe/" target="_blank" title="Voir le post instagram" rel="noreferrer noopener">
+                <span class="screen-reader-text">
+                    Description instagram
+                </span>
+                <img src="<?= get_template_directory_uri(); ?>/img/insta-quatre.jpg" alt="Femme en lingerie allongée et courbant le dos">
+            </a>
+        </li>
+        <li>
+            <a href="https://www.instagram.com/chez.olympe/" target="_blank" title="Voir le post instagram" rel="noreferrer noopener">
+                <span class="screen-reader-text">
+                    Description instagram
+                </span>
+                <img src="<?= get_template_directory_uri(); ?>/img/insta-cinq.jpg" alt="Femme nue allongée les jambes relevées">
+            </a>
+        </li>
+        <li>
+            <a href="https://www.instagram.com/chez.olympe/" target="_blank" title="Voir le post instagram" rel="noreferrer noopener">
+                <span class="screen-reader-text">
+                    Description instagram
+                </span>
+                <img src="<?= get_template_directory_uri(); ?>/img/insta-six.jpg" alt="Femme posant nue avec un drap entre les jambes">
+            </a>
+        </li>
+        <li>
+            <a href="https://www.instagram.com/chez.olympe/" target="_blank" title="Voir le post instagram" rel="noreferrer noopener">
+                <span class="screen-reader-text">
+                    Description instagram
+                </span>
+                <img src="<?= get_template_directory_uri(); ?>/img/insta-deux.jpg" alt="Deux femme allongées dans un lit en chemise">
+            </a>
+        </li>
+    </ul>
+</section>
+
+<section class="benefits product-benefits">
+    <div class="large-container">
+        <ul class="benefits-list">
+            <li>
+                <img src="<?= get_template_directory_uri(); ?>/img/camion.svg" alt="Illustration d'un camion">
+                <p>Livraison rapide et gratuite dès 99€</p>
+            </li>
+            <li>
+                <img src="<?= get_template_directory_uri(); ?>/img/cb.svg" alt="Illustration d'une carte bleue">
+                <p>Paiement sécurisé</p>
+            </li>
+            <li>
+                <img src="<?= get_template_directory_uri(); ?>/img/cils.svg" alt="Illustration de cils">
+                <p>Livraison discrète</p>
+            </li>
+            <li>
+                <img src="<?= get_template_directory_uri(); ?>/img/check.svg" alt="Illustration d'une main check">
+                <p>Satisfaite ou remboursée</p>
+            </li>
+        </ul>
+    </div>
+</section>
--- single.php
+++ single.php
@@ -43,6 +43,9 @@ json_encode($post_list);
 ?>
 
 <main id="single">
+    <?php $current_categories = get_the_category();
+    $current_title = get_the_title();
+    ?>
     <section class="hero-banner">
         <div class="leaf-top"></div>
         <h1>Le blog d'Olympe</h1>
@@ -76,6 +79,26 @@ json_encode($post_list);
             </aside>
         </div>
     </section>
+    <section class="also-like">
+        <div class="container">
+            <h3>Vous aimerez aussi :</h3>
+            <div class="post-same">
+                <div class="post-content__post-same" v-for="post in alsoLike">
+                    <div class="img__post">
+                        <a :href="post.permalink" v-html="post.thumbnails" :title="post.title"></a>
+                    </div>
+                    <div class="post__text">
+                        <h4>{{post.title}}</h4>
+                        <ul>
+                            <li v-for="category in post.categories">{{category.name}}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php get_template_part('/parts/bottom'); ?>
 
 
     <script>
@@ -87,21 +110,40 @@ json_encode($post_list);
             data() {
                 return {
                     data: null,
+                    currentCategories: null,
+                    currentTitle: null,
                     mostReaded: null,
+                    alsoLike: null,
                 }
             },
             async mounted() {
                 await this.getData();
                 this.mostReadArticle();
+                this.setAlsoLike();
             },
             methods: {
                 getData() {
-                    this.data = <?= json_encode($post_list); ?>;
+                    this.data = <?php echo json_encode($post_list); ?>;
+                    this.currentCategories = <?php echo json_encode($current_categories); ?>;
+                    this.currentTitle = <?php echo json_encode($current_title); ?>;
                 },
                 mostReadArticle() {
                     this.mostReaded = this.data.filter((item) => {
                         return item.mostreaded === "Oui";
                     });
+                },
+                setAlsoLike() {
+                    let tempArray = []
+                    this.currentCategories.forEach((category) => {
+                        this.data.forEach(element => {
+                            element.categories.forEach((item) => {
+                                if (item.name === category.name && element.title !== this.currentTitle) {
+                                    tempArray.push(element);
+                                }
+                            })
+                        })
+                    });
+                    this.alsoLike = tempArray;
                 }
             }
         }).mount('#single')
