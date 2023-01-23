e96fe821f88f9db7f37fe8b0153f4f2da49c5dfd
Tuvedunyel <mightygodx@gmail.com>
2023-01-23 09:34:41 +0100

    retour 1

---
+++ template-legals.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<main id="legals">
+    <div class="container">
+        <h1><?php the_title(); ?></h1>
+        <article class="content">
+            <?php the_content(); ?>
+        </article>
+    </div>
+</main>
+
+<?php
+get_template_part('./parts/bottom');
+get_footer(); ?>
